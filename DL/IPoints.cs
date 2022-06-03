70e9e53a8e1fe7f0fcae61c9f6537a7dcffdc273
Sletts <Qopster@gmail.com>
2022-06-03 12:31:00 -0700

    Added interfaces for DL

---
+++ DL/IPoints.cs
@@ -0,0 +1,15 @@
+﻿using EFPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public interface IPoints
+    {
+        //Update by user name
+        PointsSystem UpdatePoints(string name);
+    }
+}
---
+++ DL/IUserRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFPractice.Models;
+namespace DL
+{
+    public interface IUserRepo
+    {
+        //Add User
+        UserProfile AddUser(UserProfile user);
+        //Update User
+        UserProfile UpdateUser(UserProfile user);
+    }
+}
