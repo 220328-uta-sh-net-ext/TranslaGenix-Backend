eae4bf9098639a2d6f9e6496bd7cda62946322db
Sletts <Qopster@gmail.com>
2022-06-03 11:11:08 -0700

    Adding Entity Framework

---
+++ EFPractice/Models/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFPractice.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<UserProfile> users { get; set; }
+
+        public DbSet<PointsSystem> points { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:translagenixserver.database.windows.net,1433;Initial Catalog=translagenixdb;Persist Security Info=False;User ID=projectthree;Password=Passw0rd123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserProfile>()
+            .HasMany(b => b.points)
+            .WithOne();
+        }*/
+    }
+}
