45dadc3243cc12e0542b02a023f306d1f74ac930
Jay Williams <j.williams2014@hotmail.com>
2022-06-03 17:24:21 -0400

    fixing the problems with EF

---
+++ DL/UserRepo.cs
@@ -0,0 +1,33 @@
+﻿using EFPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class UserRepo : IUserRepo
+    {
+        private Context db;
+
+        public UserRepo (Context db)
+        {
+            this.db = db;
+        }
+
+        public UserProfile AddUser(UserProfile user)
+        {
+            db.users.Add(user);
+            db.SaveChanges();
+            return user;
+        }
+
+        public UserProfile UpdateUser(UserProfile user)
+        {
+            db.users.Update(user);
+            db.SaveChanges();
+            return user;
+        }
+    }
+}
