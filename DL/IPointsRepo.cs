90885f4e5c985026ff086bb14872da7acdd06b63
Sletts <Qopster@gmail.com>
2022-06-06 08:10:03 -0700

    Added DL for Points

---
+++ DL/IPointsRepo.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+
+namespace DL
+{
+    public interface IPointsRepo
+    {
+        Point AddPoint(Point point);
+        List<Point> GetAllPoints();
+        Point GetHighestPoint();
+        Point GetPointById(int id);
+        Point GetPointByUserName(string UserName);
+        Point UpdatePoints(Point point);
+        Point IncreasePointsById(int id);
+        Point IncreasePointsByUserName(string username);
+        void DeletePointbyID(int id);
+        void DeletePointbyUserName(string name);
+    }
+}
