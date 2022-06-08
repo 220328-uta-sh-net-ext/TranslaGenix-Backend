fdb9f79c9ea96624b3fd9a0e2802b12b027beb67
Sletts <Qopster@gmail.com>
2022-06-08 11:09:19 -0700

    Added SimpleUser for Kevin

---
+++ Models/SimpleUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Models
+{
+    [Table("simpleuser")]
+    [Index(nameof(Username), IsUnique = true)]
+    public class SimpleUser
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required]
+        [Column("username")]
+        public string Username { get; set; }
+        [Required]
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ TranslaGenixAPI/Controllers/SimpleUserController.cs
@@ -0,0 +1,55 @@
+﻿using DL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Models;
+
+namespace TranslaGenixAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleUserController : ControllerBase
+    {
+        private ISimpleUserRepo repo;
+        private static List<SimpleUser> users = new List<SimpleUser>();
+        public SimpleUserController(ISimpleUserRepo repo)
+        {
+            this.repo = repo;
+        }
+        [HttpGet]
+        [Route("GetAllSimpleUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<User>> Get()
+        {
+            try
+            {
+                users = repo.GetAllSimpleUsers();
+            }
+            catch (Exception ex)
+            {
+                //Log.Information("Bad Request exception @ Get User in UserC.");
+                return BadRequest(ex.Message);
+            }
+            //Log.Information("Good request @ Get User in Userc.");
+            return Ok(users);
+        }
+        [HttpPost]
+        [Route("AddSimpleUser")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult AddNewUser([FromQuery][BindRequired] SimpleUser user)
+        {
+            //Try to add a new user
+            try
+            {
+                repo.AddUser(user);
+            }
+            catch (Exception ex)
+            {
+                //Log.Information("Excetion occured in @ AddNewUser in UserC: " + ex);
+                return BadRequest("Bad Request: " + ex);
+            }
+            //Log.Information("New user created w/ username: " + UserName + " @ AddNewUser in UserC");
+            return CreatedAtAction("Get", user);
+        }
+    }
+}
