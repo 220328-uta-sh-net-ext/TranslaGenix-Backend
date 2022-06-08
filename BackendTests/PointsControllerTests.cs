50c4707b098abde1f0fd2fb75d3c17dea83bee6f
Sletts <Qopster@gmail.com>
2022-06-08 11:56:17 -0700

    Added one test that kinda works.

--- BackendTests/PointsControllerTests.cs
+++ BackendTests/PointsControllerTests.cs
@@ -1,4 +1,12 @@
-﻿using Xunit;
+﻿using DL;
+using FakeItEasy;
+using Xunit;
+using Models;
+using TranslaGenixAPI.Controllers;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BackendTests
 {
@@ -7,7 +15,10 @@ namespace BackendTests
         [Fact]
         public void Test1()
         {
-
+            var fakeContext = A.Fake<TGContext>();
+            PointsRepo pr = new PointsRepo(fakeContext);
+            var PointCheck = new PointsController(pr);
+            Assert.NotNull(PointCheck);
         }
     }
 }
