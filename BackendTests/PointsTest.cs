1cc4caa64ba128b4a8a8f930f7a431510379d612
Markfon <m4rkfons@outlook.com>
2022-06-13 14:10:53 -0400

    Unity Test

---
+++ BackendTests/PointsTest.cs
@@ -0,0 +1,29 @@
+﻿using DL;
+using FakeItEasy;
+using Xunit;
+using Models;
+using TranslaGenixAPI.Controllers;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Net;
+
+namespace BackendTests
+{
+    public class PointsTests
+    {
+
+        [Fact]
+        public void Get_AllPoints()
+        {
+            var fakeContext = A.Fake<TGContext>();
+            PointsRepo pr = new PointsRepo(fakeContext);
+
+            var ret = pr.GetAllPoints();
+            Assert.NotNull(ret);
+        }
+
+    }
+}
