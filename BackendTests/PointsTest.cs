d1c5cc056186380188fdbaf80713970acb193850
Sletts <Qopster@gmail.com>
2022-06-14 07:47:25 -0700

    Added Pointer Controller and DL tests.

--- BackendTests/PointsTest.cs
+++ BackendTests/PointsTest.cs
@@ -14,7 +14,7 @@ namespace BackendTests
     public class PointsTests
     {
         [Fact]
-        public void Get_AllPoints()
+        public void GetAllPointsTest()
         {
             var mockRepo = new Mock<IPointsRepo>();
             mockRepo.Setup(x => x.GetAllPoints()).Returns(new List<Point>());
@@ -24,7 +24,7 @@ namespace BackendTests
         }
 
         [Fact]
-        public void Add_point()
+        public void AddpointTest()
         {
             var mockRepo = new Mock<IPointsRepo>();
             var mockPoint = new Point();
@@ -34,7 +34,7 @@ namespace BackendTests
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
         }
         [Fact]
-        public void Get_HighestPoint()
+        public void GetHighestPointTest()
         {
             var mockRepo = new Mock<IPointsRepo>();
             var mockPoint = new Point();
@@ -45,7 +45,7 @@ namespace BackendTests
         }
 
         [Fact]
-        public void Get_PointById()
+        public void GetPointByIdTest()
         {
             var mockRepo = new Mock<IPointsRepo>();
             var mockPoint = new Point();
@@ -58,19 +58,100 @@ namespace BackendTests
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
         }
 
-        /*[Fact]
-        public void Get_PointByUserName()
+        [Fact]
+        public void GetPointByUserNameTest()
         {
             var mockRepo = new Mock<IPointsRepo>();
             var mockPoint = new Point();
             mockPoint.Id = 1;
             mockPoint.userId = 1;
             mockPoint.Points = 0;
-            mockRepo.Setup(x => x.GetPointById(It.IsAny<int>())).Returns(mockPoint);
-            mockRepo.Setup()
+            mockRepo.Setup(x => x.GetPointByUserName(It.IsAny<string>())).Returns(mockPoint);
             var mockCont = new PointsController(mockRepo.Object);
-            ActionResult actionResult = mockCont.GetPointById(1);
+            ActionResult actionResult = mockCont.GetPointByUserName("test");
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
-        }*/
+        }
+
+        [Fact]
+        public void UpdatePointsTest()
+        {
+            var mockRepo = new Mock<IPointsRepo>();
+            var mockPoint = new Point();
+            mockPoint.Id = 1;
+            mockPoint.userId = 1;
+            mockPoint.Points = 0;
+            mockRepo.Setup(x => x.UpdatePoints(mockPoint)).Returns(mockPoint);
+            var mockCont = new PointsController(mockRepo.Object);
+            ActionResult actionResult = mockCont.UpdatePoints("test", 5);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
+        }
+
+        [Fact]
+        public void IncreasePointsByIdTest()
+        {
+            var mockRepo = new Mock<IPointsRepo>();
+            var mockPoint = new Point();
+            mockPoint.Id = 1;
+            mockPoint.userId = 1;
+            mockPoint.Points = 0;
+            mockRepo.Setup(x => x.IncreasePointsById(It.IsAny<int>())).Returns(mockPoint);
+            var mockCont = new PointsController(mockRepo.Object);
+            ActionResult actionResult = mockCont.IncreasePointsById(1);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
+        }
+
+        [Fact]
+        public void IncreasePointsByUserNameTest()
+        {
+            var mockRepo = new Mock<IPointsRepo>();
+            var mockPoint = new Point();
+            mockPoint.Id = 1;
+            mockPoint.userId = 1;
+            mockPoint.Points = 0;
+            mockRepo.Setup(x => x.IncreasePointsByUserName(It.IsAny<string>())).Returns(mockPoint);
+            var mockCont = new PointsController(mockRepo.Object);
+            ActionResult actionResult = mockCont.IncreasePointsByUserName("test");
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
+        }
+
+        [Fact]
+        public void DeletePointbyIDTest()
+        {
+            var mockRepo = new Mock<IPointsRepo>();
+            var mockPoint = new Point();
+            mockPoint.Id = 1;
+            mockPoint.userId = 1;
+            mockPoint.Points = 0;
+            mockRepo.Setup(x => x.DeletePointbyID(It.IsAny<int>()));
+            var mockCont = new PointsController(mockRepo.Object);
+            ActionResult actionResult = mockCont.DeletePointbyID(1);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
+        }
+
+        [Fact]
+        public void DeletePointbyUserName()
+        {
+            var mockRepo = new Mock<IPointsRepo>();
+            var mockPoint = new Point();
+            mockPoint.Id = 1;
+            mockPoint.userId = 1;
+            mockPoint.Points = 0;
+            mockRepo.Setup(x => x.DeletePointbyUserName(It.IsAny<string>()));
+            var mockCont = new PointsController(mockRepo.Object);
+            ActionResult actionResult = mockCont.DeletePointbyUserName("test");
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
+        }
+
+        [Fact]
+        public void GetLeaderBoard()
+        {
+            var mockRepo = new Mock<IPointsRepo>();
+            var pointL = new List<Point>();
+            var mockPoint = new Point();
+            mockRepo.Setup(x => x.GetAllPoints()).Returns(pointL);
+            var mockCont = new PointsController(mockRepo.Object);
+            ActionResult actionResult = mockCont.GetLeaderBoard();
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(actionResult);
+        }
     }
 }
