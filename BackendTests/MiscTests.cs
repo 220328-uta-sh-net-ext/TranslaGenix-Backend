4ff18ab0fef2bcb089305bd3e5d9b0952fda43f8
Sletts <Qopster@gmail.com>
2022-06-14 12:15:06 -0700

    Added 2 Misc Tests.

---
+++ BackendTests/MiscTests.cs
@@ -0,0 +1,33 @@
+﻿using DL;
+using FakeItEasy;
+using Xunit;
+using Models;
+using TranslaGenixAPI.Controllers;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BackendTests
+{
+    public class PointsControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var fakeContext = A.Fake<TGContext>();
+            PointsRepo pr = new PointsRepo(fakeContext);
+            var PointCheck = new PointsController(pr);
+            Assert.NotNull(PointCheck);
+        }
+        [Fact]
+        public async Task Test2()
+        {
+            var fakeContext = A.Fake<TGContext>();
+            PointsRepo pr = new PointsRepo(fakeContext);
+            var PointCheck = new PointsController(pr);
+            Assert.NotNull(PointCheck);
+
+        }
+    }
+}
